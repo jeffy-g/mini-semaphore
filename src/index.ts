7a9bbd34f3530db9f0497d70d5e2d4cd386b29cb
jeffy-g <hirotom1107@gmail.com>
2022-05-15 18:53:40 +0900

    v1.3.10

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ export { MiniSemaphore } from "./class";
 export { create } from "./object";
 export { Deque } from "./deque";
 export { restrictor } from "./flow-restrictor";
-export const version = "v1.3.9";
+export const version = "v1.3.10";
 // DEVNOTE: export * as ns Syntax - since ts v3.8
 // export * as restrictor from "./flow-restrictor";
