b3c9402bb6ed97489235eac1dd0dbb77887585fd
jeffy-g <hirotom1107@gmail.com>
2023-10-24 19:48:37 +0900

    jest.config.js - fix ts-jest[ts-jest-transformer] (WARN)

--- jest.config.js
+++ jest.config.js
@@ -3,18 +3,29 @@
 //
 module.exports = {
   preset: "ts-jest/presets/default-esm", // MUST
-  globals: {
-    "ts-jest": {
-      // tsconfig: "./src/tsconfig.json", // <- needless
-      useESM: true
-    }
-  },
+  /*
+   * ts-jest[ts-jest-transformer] (WARN) Define `ts-jest` config under `globals` is deprecated. Please do
+   * transform: {
+   *     <transform_regex>: ['ts-jest', {
+   *        // ts-jest config goes here in Jest
+   *     }
+   *   ],
+   * }
+   */
+  // globals: {
+  //   "ts-jest": {
+  //     // tsconfig: "./src/tsconfig.json", // <- needless
+  //     useESM: true
+  //   }
+  // },
   // this is optional?
   testEnvironment: "node",
   verbose: true,
   // `transform` property implicitly sets preset to "ts-jest"
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.tsx?$": [
+      "ts-jest", { useESM: true }
+    ]
   },
   // default: (/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$
   testRegex: "/__tests__/.*\\.tsx?$",
