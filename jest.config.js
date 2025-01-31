bd0b30b24dd0c7c35df687b880ed17c5a103328e
jeffy-g <hirotom1107@gmail.com>
2025-01-31 12:19:51 +0900

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   // default: (/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$
   // testRegex: "/__tests__/stress-test.ts",
-  testRegex: "/__tests__/.*\\.tsx?$",
+  testRegex: "/__tests__/.*test\\.tsx?$",
   collectCoverageFrom: [
     // all ?
     "./src/!(*.d.ts)",
